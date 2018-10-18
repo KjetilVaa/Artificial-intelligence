f41f99d8ef2f507502d70067540fc0249f95f697
Kjetil Vaagen <kjetivaa@stud.ntnu.no>
2018-10-17 15:49:42 -1000

    Modified perceptron to generate and label data automatically.

--- Perceptron/Main.py
+++ Perceptron/Main.py
@@ -1,7 +1,7 @@
 from Perceptron import Perceptron
-from feedForward import feedForward
+#from feedForward import feedForward
 from numpy import array, dot
-
+import random
 
 
 def main(data, choice):
@@ -9,53 +9,87 @@ def main(data, choice):
     if choice == "P":
         total_training_sess = int(input("How many total traning session do you want to train the network? "))
         t = 0
-        and_errors = 0
-        or_errors = 0
+        errors = 0
+
         while t < total_training_sess:
             t += 1
-            ## Initializing and training AND perceptron
-            print(str(t) + "-"*20+"TRAINING AND PERCEPTRON"+"-"*20)
-            perceptron_and = Perceptron(data[0])
+            ## Initializing and train perceptron
+            print(str(t) + "-"*20+"TRAINING PERCEPTRON"+"-"*20)
+            perceptron = Perceptron(data)
 
-            ## Initializing and training OR perceptron
-            print(str(t) + "-"*20+"TRAINING OR PERCEPTRON"+"-"*20)
-            perceptron_or = Perceptron(data[1])
-
-            print(str(t) + "-"*20+"TESTING AND PERCEPTRON"+"-"*20)
-            if test(perceptron_and, AND):
-                and_errors += 1
-            print(str(t) + "-"*20+"TESTING OR PERCEPTRON"+"-"*20)
-            if test(perceptron_or, OR):
-                or_errors += 1
+            print(str(t) + "-"*20+"TESTING PERCEPTRON"+"-"*20)
+            if test(perceptron, testData):
+                errors += 1
 
         print("-"*20 + "TRAINING AND TESTING COMPLETE"+"-"*20)
-        print("AND_ERROS: %s, OR_ERRORS: %d, ITERATIONS: %f" % (and_errors, or_errors, total_training_sess))
+        print("ERRORS: %d, ITERATIONS: %f" % (errors, total_training_sess))
     elif choice == "F":
-        feedForwardNetwork = feedForward()
+        print("Pyprain not currently availiable")
+        #feedForwardNetwork = feedForward()
     else:
         print("Invalid input. Running percetron by default...")
         main(data, "P")
 
 
-
-
+def testPrediction(inputs, weights, thres):
+    a = dot(inputs, weights)
+    if a >= thres:
+        return 1
+    else:
+        return -1
 
 def test(p, info):
     isError = False
-    for win, ed in info:
-        res = dot(win, p.weights) > p.theta
-        diff = ed - res
+    for inputs, truth in info:
+        prediction = testPrediction(inputs, p.weights, p.theta)
+        diff = truth - prediction
         if diff != 0:
-            print("Error: input %s with output %d" % (repr(win), ed))
+            print("Error: input %s with output %d" % (repr(inputs), truth))
             isError = True
         else:
-            print("Test passed! input %s with output %d" % (repr(win), ed))
+            print("Test passed! input %s with output %d" % (repr(inputs), truth))
     return isError
 
+
+def getRandomNumber():
+    return int(round(random.uniform(-10, 10)))
+
+def calculateLabel(x, y):
+    #Label function: f(x) = 2x
+    #Above line is 1, and below is -1 ---> check prediction helper function
+    func = 2*x
+    if func > y:
+        return (array([x, y]), -1)
+    elif func < y:
+        return (array([x, y]), 1)
+    else:
+        #discard
+        return calculateLabel(getRandomNumber(), getRandomNumber())
+
+def generateData(numPoints, outputMsg):
+    print("Generating data")
+    data = []
+    i = 1
+    while i <= numPoints:
+        i+= 1
+        x = getRandomNumber()
+        y = getRandomNumber()
+        point = calculateLabel(x, y)
+        if outputMsg:
+            print("Point %s with label %s added." % (point[0], point[1]))
+        data.append(point)
+    return data
 
 
 if __name__ == "__main__":
 
+    numPoints = int(input("How many data points do you want? "))
+    
+    #Generate data
+    data = generateData(numPoints, True)
+    testData = generateData(numPoints, False)
+    
+    """
     AND = [
         (array([0, 0]), 0),
         (array([0, 1]), 0),
@@ -69,8 +103,7 @@ if __name__ == "__main__":
         (array([1, 0]), 1),
         (array([1, 1]), 1),
     ]
-
-    data = array([AND, OR])
+    """
 
     print("TO RUN THE PERCEPTRON PRESS 'P'")
     print("TO RUN THE FEEDFORWARD ANN PRESS 'F'")
--- Perceptron/Perceptron.py
+++ Perceptron/Perceptron.py
@@ -5,24 +5,31 @@ from numpy import array, dot
 class Perceptron(object):
 
 
-    def __init__(self, data, lrate=0.2, theta=5, amountWeights=2):
+    def __init__(self, data, lrate=0.2, theta=0, amountWeights=2):
         self.theta = theta
         self.lrate = lrate
         self.weights = array([self.getRandomInterval() for x in range(amountWeights)])
         self.train(data)
+
+    def predict(self, inputs, weights):
+        a = dot(inputs, weights)
+        if a >= self.theta:
+            return 1
+        else:
+            return -1
 
     def train(self, data):
         iteration = 0
         while True:
             iteration+= 1
             mistakes = 0
-            for win, ed in data:
-                res = dot(win, self.weights) > self.theta
-                diff = ed - res
+            for inputs, truth  in data:
+                prediction = self.predict(inputs, self.weights)
+                diff = truth - prediction
                 if diff != 0:
                     mistakes+= 1
                     ##Formula for reweighting
-                    self.weights = array([self.weights[i] + self.lrate * diff * value for i, value in enumerate(win)])
+                    self.weights = array([self.weights[i] + self.lrate * diff * value for i, value in enumerate(inputs)])
                     print("Vector change: %s" % repr(self.weights))
             if not mistakes:
                 break
